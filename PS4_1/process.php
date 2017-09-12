d31734a3b5ecdb2f685cc5d4f1335db8621e2fc2
Kasun Gamage <kasun.gamage@webstasy.one>
2017-09-12 20:59:36 +0530

    PHP extras

---
+++ PS4_1/process.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      //echo 1 + 5;
+      //print_r($_GET);
+      $x = $_POST['reg'];
+      $y = $_POST['student-name'];
+
+      echo '<p>registration number is ' . $x . '</p><br>';
+      echo '<p>student name is ' . $y . '</p><br>';
+
+      $conn = mysqli_connect("localhost", "root", "", "newdb");
+      $sql = "INSERT INTO student VALUES('$x', '$y')";
+      $sqlSingleQuote = 'INSERT INTO student VALUES(' . $x . ',' . $y . ')';
+
+      $conn->query($sql);
+      echo mysqli_error($conn);
+
+      mysqli_close($conn);
+
+    ?>
+  </body>
+</html>
---
+++ PS4_1/student.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2017 at 10:32 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `regno` varchar(50) NOT NULL,
+  `student_name` varchar(50) NOT NULL
+);
